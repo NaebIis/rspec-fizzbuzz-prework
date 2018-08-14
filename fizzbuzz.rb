5a32b858eb0f8d1cb2a4532c3b78debb5047eef7
John Grubbs <johnmlowery@gmail.com>
2018-08-14 18:40:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(n) 
-  if n % 3 == 0 
+  if n % 3 == 0 && n % 5 == 0 
       return"Fizz"
     elsif n % 5 == 0
       return "Buzz"
-    elsif n % 3 == 0 && n % 5 == 0 
+    elsif n % 3 == 0 
       return "FizzBuzz"
     else  
       return nil
