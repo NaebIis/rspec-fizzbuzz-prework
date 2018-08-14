c095f28001b514c9295d6491c6a52c732eb40aa2
John Grubbs <johnmlowery@gmail.com>
2018-08-14 17:56:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,13 @@
 def fizzbuzz(n) 
-remainderWhenDividedBy3 = n % 3 
+remainderWhenDividedBy3 = n % 3
+remainderWhenDividedBy5 = n % 5
+remainderWhenDividedBy15 = n % 15
     if remainderWhenDividedBy3 == 0 
-    return"Fizz"
+      return"Fizz"
+    elsif remainderWhenDividedBy5 == 0
+      return "Buzz"
+    elsif remainderWhenDividedBy15 == 0 
+      return "FizzBuzz"
   else
     return false
   end
